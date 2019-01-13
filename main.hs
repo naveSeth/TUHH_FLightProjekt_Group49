6c251f31136d1b7aad80dfb917ce7b9868068c0e
naveSeth <40666654+naveSeth@users.noreply.github.com>
2019-01-13 15:46:52 +0100

    Delete main.hs

--- main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-import Network.MateLight.Simple
-
-import Data.Maybe
-import qualified Network.Socket as Sock
-
-move :: (Int, Int) -> String -> (Int,Int) -> (Int, Int)
-move (xdim,ydim) '
