0f00b0af7df208839194ef1ae890eb3b4a05edb8
WhiterFighter99 <46650114+WhiterFighter99@users.noreply.github.com>
2019-01-20 18:07:13 +0100

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -5,17 +5,21 @@ import Data.Maybe
 import qualified Network.Socket as Sock
 
 move :: (Int, Int) -> String -> (Int, Int) -> (Int, Int)
-move (xdim, ydim) "\"j\"" (x, y) = (x, (y + 1) `mod` ydim)
-move (xdim, ydim) "\"k\"" (x, y) = (x, (y - 1) `mod` ydim)
-move (xdim, ydim) "\"h\"" (x, y) = ((x - 1) `mod` xdim, y)
-move (xdim, ydim) "\"l\"" (x, y) = ((x + 1) `mod` xdim, y)
+move (xdim, ydim) "\"s\"" (x, y) = (x, (y + 1) `mod` ydim)
+move (xdim, ydim) "\"w\"" (x, y) = (x, (y - 1) `mod` ydim)
+move (xdim, ydim) "\"a\"" (x, y) = ((x - 1) `mod` xdim, y)
+move (xdim, ydim) "\"d\"" (x, y) = ((x + 1) `mod` xdim, y)
 move _ _ x = x
 
 toFrame :: (Int, Int) -> (Int, Int) -> ListFrame
-toFrame (xdim, ydim) (x', y') = ListFrame $ map (\y -> map (\x -> if x == x' && y == y' then Pixel 0xff 0xff 0xff else Pixel 0 0 0) [0 .. xdim - 1]) [0 .. ydim - 1]
+toFrame (xdim, ydim) (x', y') = ListFrame $ map (\y -> map (\x -> if x == x' && (y == y' || y == y'+1 || y== y'-1) then Pixel 0xff 0xff 0xff else Pixel 0 0 0) [0 .. xdim - 1]) [0 .. ydim - 1]
 
 eventTest :: [Event String] -> (Int, Int) -> (ListFrame, (Int, Int))
 eventTest events pixel = (toFrame dim pixel', pixel')
+  where pixel' = foldl (\acc (Event mod ev) -> if mod == "KEYBOARD" then move dim ("\"s\"") acc else acc) pixel events
+  
+eventTest' :: [Event String] -> (Int, Int) -> (ListFrame, (Int, Int))
+eventTest' events pixel = (toFrame dim pixel', pixel')
   where pixel' = foldl (\acc (Event mod ev) -> if mod == "KEYBOARD" then move dim ev acc else acc) pixel events
 
 dim :: (Int, Int)
